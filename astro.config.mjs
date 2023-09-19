70a68695da8ad920f0fac8db1fc6849ed229613a
Niklas Resch <github@datentopf.org>
2023-09-19 23:21:43 +0200

    Initial config for new theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  // output: 'server',
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true,
+  //   },
+  // }),
   // used to generate images
   site:
     process.env.VERCEL_ENV === 'production'
