2faa879d6fc2ac0bd3a1ec2f0bb2f0515c3d22b7
Niklas Resch <github@datentopf.org>
2023-09-22 15:53:47 +0200

    Add cloudflare ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,18 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
-
 import solidJs from "@astrojs/solid-js";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.niklas.fyi/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://www.niklas.fyi/sitemap-index.xml",
-        "https://www.niklas.fyi/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-  ],
+  integrations: [sitemap(), robotsTxt({
+    sitemap: ["https://www.niklas.fyi/sitemap-index.xml", "https://www.niklas.fyi/sitemap-0.xml"]
+  }), solidJs(), UnoCSS({
+    injectReset: true
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
