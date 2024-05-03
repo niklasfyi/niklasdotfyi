821306252e6174ba3acca57567a6f8f71b5455b1
Niklas Resch <github@datentopf.org>
2024-05-04 00:42:18 +0200

    Add access token check to micropub

--- src/pages/micropub.ts
+++ src/pages/micropub.ts
@@ -1,3 +1,7 @@
+import type { APIRoute } from 'astro';
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+
 export const prerender = false;
 
 const micropubConfig = {
@@ -162,7 +166,10 @@ const micropubConfig = {
   ]
 };
 
-export async function GET({ request }: { params: any, request: Request }) {
+export const GET: APIRoute = async ({ request }) => {
+
+  checkAccessToken(request);
+
   const url = new URL(request.url);
   const searchParams = new URLSearchParams(url.search);
 
@@ -190,21 +197,73 @@ export async function GET({ request }: { params: any, request: Request }) {
   }
 }
 
+export const POST: APIRoute = async ({ request }) => {
+  const cat_result = checkAccessToken(request);
+  const cat_status = (await cat_result).status;
+  if (cat_status === 400 || cat_status === 401) {
+    return cat_result;
+  } 
+  if (request.headers.get("Content-Type") === "application/json") {
+    const body = await request.json();
+    const name = body.name;
+    return new Response(JSON.stringify({
+      message: "Your name was: " + name
+    }), {
+      status: 200
+    })
+  }
+  return new Response(null, { status: 200 });
+}
 
-// export async function POST({ params, request }: { params: any, request: Request }) {
-//   const data = await request.json();
-//   console.log(data);
-//   // const post = createPost(data);
-//   // return new Response(
-//   //   null,
-//   //   {
-//   //     status: 201,
-//   //     headers: {
-//   //       'Location': `/posts/${post.slug}`
-//   //     }
-//   //   }
-//   // );
-// }
+async function checkAccessToken(request: Request): Promise<Response>{
+
+  const access_token = (await request.formData()).get('access_token');
+  const auth_header = request.headers.get('Authorization');
+  // Check if both access token and authorization header are present
+  if (access_token && auth_header) {
+    return new Response('Only one method of authentication is allowed', { status: 400 });
+  } 
+  // Check if neither access token nor authorization header are present
+  if (!access_token && !auth_header) {
+    return new Response('No authentication method provided', { status: 401 });
+  }
+  // Check if only one of access token or authorization header is present and set the access token accordingly
+  let accessToken = access_token ? access_token : auth_header?.substring(7);
+
+  const token_endpoint = await discoverEndpoints('https://www.niklas.fyi/');
+
+  if (!token_endpoint) {
+    return new Response('No token endpoint found', { status: 400 });
+  }
+
+  // Call the token endpoint with the access token
+  const scope_response = await fetch(token_endpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + accessToken,
+    }});
+
+  if (scope_response.status !== 200) {
+    return new Response('Invalid access token', { status: 401 });
+  }
+  return new Response(null, { status: 200 });
+}
+
+async function discoverEndpoints(url: string, rel: string = 'token_endpoint') {
+  const response = await fetch(url, { method: 'GET' });
+  const body = await response.text();
+  
+  // Load the HTML content into cheerio
+  const $ = cheerio.load(body);
+
+  // Select all link tags with the specified rel attribute and map them to an array of their href attributes
+  const linkTags = $(`link[rel="${rel}"]`).map((_, link) => $(link).attr('href')).get();
+
+  console.log(linkTags);
+  return linkTags[0];
+}
+
 
 function queryHandler(query: string) {
   if (query === 'config') {
