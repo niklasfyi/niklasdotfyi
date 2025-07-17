7b14caa6eb84092fb568e197c5cffeaa3c42e52e
Niklas Siefke <niklas@mazemap.com>
2025-07-17 13:12:32 +0200

    Add markdown-it and sanitize-html, update RSS feed logic
    - Add `markdown-it` and `sanitize-html` dependencies (and types)
    - Update main RSS feed to render and sanitize Markdown content
    - Improve RSS item titles for bookmarks and checkins
    - Remove unused notes RSS feed
    - Update Preview component to show "Bookmarked"/"At" prefixes
    - Move bookmark file to dated folder structure

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,25 @@
 import { getAllEntries } from "@/data/entry";
 import { siteConfig } from "@/site.config";
 import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+import type { CollectionEntryType } from "@/types";
+
+// function to return title
+function getTitle(entry: CollectionEntryType): string {
+	console.log(entry);
+	if ("title" in entry.data && entry.data.title) {
+		if ("bookmark-of" in entry.data && entry.data["bookmark-of"].length > 0) {
+			return `Bookmarked ${entry.data.title}`;
+		}
+		return entry.data.title;
+	} else if ("checkin" in entry.data && entry.data.checkin.length > 0) {
+		return `At ${entry.data.checkin?.[0]?.properties?.name?.[0]}`;
+	} else {
+		return entry.body?.slice(0, 30) + "..." || "Untitled";
+	}
+}
 
 export const GET = async () => {
 	const entries = await getAllEntries();
@@ -9,14 +28,18 @@ export const GET = async () => {
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
-		items: entries.map((entry) => ({
-			title:
-				"title" in entry.data && entry.data.title
-					? entry.data.title
-					: entry.body?.slice(0, 30) + "..." || "Untitled",
-			description: entry.data.description,
-			pubDate: entry.data.date,
-			link: `${entry.id}`,
-		})),
+		items: await Promise.all(
+			entries.map(async (entry) => ({
+				title: getTitle(entry),
+				description: entry.data.description,
+				pubDate: entry.data.date,
+				link: `${entry.id}`,
+				content: entry.body
+					? sanitizeHtml(parser.render(entry.body), {
+							allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+						})
+					: undefined,
+			})),
+		),
 	});
 };
